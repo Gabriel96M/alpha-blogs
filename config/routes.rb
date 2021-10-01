25818e05f134001a859cfafbe8424591d8456e39
Pop Gabriel <gabryel96@yahoo.com>
2021-10-01 15:06:58 +0300

    Styling the project

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	resources :users
   	resources :articles
   	#get 'pages/about'
-	root 'articles#index'
+	root 'pages#home'
 	get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
